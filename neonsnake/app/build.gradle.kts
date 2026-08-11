070e8f297003329529fa60ae3b9306dc76623844
ZFR.v2 <yorgancizafer1@gmail.com>
2026-08-12 02:14:39 +0300

    Update build.gradle.kts

--- neonsnake/app/build.gradle.kts
+++ neonsnake/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.refaz.neonsnake"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1"
+        versionCode = 6
+        versionName = "1.2"
         manifestPlaceholders["admobAppId"] = "ca-app-pub-8054232338509216~1128866919"
     }
     signingConfigs {
